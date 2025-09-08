4b2c5b130a1a3a97649f372308a7590f3564d502
Hajimalung Baba <hajimalungbaba.786@gmail.com>
2025-09-08 08:57:44 +0530

    header in app component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,12 @@
 import { Component, signal } from '@angular/core';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [MatToolbarModule, MatIconModule, MatButtonModule, MatBadgeModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
